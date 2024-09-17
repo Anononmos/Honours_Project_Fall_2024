19dc9c33c86f6a838b104e1614a21101d79d6b92
Anononmos <ainan.kashif@gmail.com>
2024-09-17 16:00:25 -0400

    Basic endpoint setup with skeleton views

--- honours_project/clip_sharing_app/views.py
+++ honours_project/clip_sharing_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """Homepage view function."""
+
+    return render(request, 'base_generic.html')
+
+def watch(request):
+    video_id = request.GET.get('v', None)
+
+    # TODO Implement check for valid video_id
