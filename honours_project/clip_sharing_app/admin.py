58d01bf3c3e1ccdd6036b0b4ea8c5182fa094456
Anononmos <ainan.kashif@gmail.com>
2024-09-22 23:44:21 -0400

    Fixed bug with upload date

--- honours_project/clip_sharing_app/admin.py
+++ honours_project/clip_sharing_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Video, VideoInstance
 
 # Register your models here.
+
+admin.site.register(Video)
+admin.site.register(VideoInstance)
--- honours_project/clip_sharing_app/models.py
+++ honours_project/clip_sharing_app/models.py
@@ -9,7 +9,7 @@ class Video(models.Model):
 
     title = models.CharField(max_length=50, blank=True, null=True, help_text='Enter a title for the video.')
     views = models.PositiveIntegerField(default=0, help_text='View count of the video.')
-    uploadDate = models.DateTimeField(editable=False, help_text='Upload date for the video.')
+    uploadDate = models.DateTimeField(auto_now_add=True, help_text='Upload date for the video.')
 
     def __str__(self) -> str:
         return f'Title: {self.title}\nUploaded: {self.uploadDate}\n {self.views} views.'
@@ -18,8 +18,8 @@ class Video(models.Model):
 class VideoInstance(models.Model):
     """Includes video file and access code."""
 
-    id = models.CharField(primary_key=True, max_length=6, default=get_random_string(length=6), editable=False, help_text='6-character alphanumeric id for video.')
-    expires = models.DateTimeField(editable=False, help_text='Expiry time of the video.')
+    id = models.CharField(primary_key=True, max_length=6, default=get_random_string(length=6), help_text='6-character alphanumeric id for video.')
+    expires = models.DateTimeField(help_text='Expiry time of the video.')
     video = models.OneToOneField(Video, on_delete=models.CASCADE)
     file = models.FileField(upload_to="uploads/")
 
