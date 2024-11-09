e2ffc861135a952366ed8df23f46df8351aeccc4
Anononmos <ainan.kashif@gmail.com>
2024-11-09 17:52:42 -0500

    Start deployment by moving secret key and debug to .env

--- honours_project/honours_project/settings.py
+++ honours_project/honours_project/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +18,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+import os
+from dotenv import load_dotenv
+
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nbuqd6&$joy19w!de#(y3@^&!4%buiiqt3e7g&l41y2-zy#o5s'
+# SECRET_KEY = 'django-insecure-nbuqd6&$joy19w!de#(y3@^&!4%buiiqt3e7g&l41y2-zy#o5s'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nbuqd6&$joy19w!de#(y3@^&!4%buiiqt3e7g&l41y2-zy#o5s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -73,6 +80,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'honours_project.wsgi.application'
 
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -120,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = ''
 
 MEDIA_ROOT = ''
 
