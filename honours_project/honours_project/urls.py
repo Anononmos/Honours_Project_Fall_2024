038630ee55ac4c50aa218e34560346007f7149ae
Anononmos <ainan.kashif@gmail.com>
2024-11-12 00:06:52 -0500

    Added admin endpoint to fix bug.

--- honours_project/honours_project/urls.py
+++ honours_project/honours_project/urls.py
@@ -23,6 +23,7 @@ from clip_sharing_app import views
 urlpatterns = [
     path('', views.index, name='index'), 
     path('watch', views.watch, name='watch'),
-    path('watch/', views.watch, name='watch'), 
+    path('watch/', views.watch, name='watch'),
+    path('admin', admin.site.urls),  
     path('admin/', admin.site.urls), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
