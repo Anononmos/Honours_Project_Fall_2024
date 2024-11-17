3b6695dfa03fa1e7167fb97adf41a9855206f154
Anononmos <ainan.kashif@gmail.com>
2024-11-17 10:57:34 -0500

    Created manage.py command to delete expired videos. Should work in daily tasks section.

--- honours_project/clip_sharing_app/delete_expired.py
+++
@@ -1,4 +0,0 @@
-from .models import delete_expired
-
-if __name__ == "__main__":
-    delete_expired()
---
+++ honours_project/clip_sharing_app/management/commands/delete_expired.py
@@ -0,0 +1,8 @@
+from django.core.management import BaseCommand
+from clip_sharing_app.models import delete_expired
+
+class Command(BaseCommand):
+    help = 'Deletes Video and VideoInstance objects from the database that are expired.'
+
+    def handle(self, *args, **options):
+        delete_expired()
