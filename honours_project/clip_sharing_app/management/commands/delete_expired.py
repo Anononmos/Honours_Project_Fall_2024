8280bee55213f034c9b9b5c3fdec326d5d91bf2b
Anononmos <ainan.kashif@gmail.com>
2024-12-03 18:36:45 -0500

    updated the delete_expired command to log the deleted videos.

--- honours_project/clip_sharing_app/management/commands/delete_expired.py
+++ honours_project/clip_sharing_app/management/commands/delete_expired.py
@@ -1,10 +1,22 @@
 from django.core.management import BaseCommand
-from clip_sharing_app.models import delete_expired
+from django.utils import timezone
+from clip_sharing_app.models import Video
 
 class Command(BaseCommand):
     help = 'Deletes Video and VideoInstance objects from the database that are expired.'
 
     def handle(self, *args, **options):
-        delete_expired()
+        expired_videos = Video.objects.filter(expires__lte=timezone.now())
 
-        self.stdout.write('Successfully deleted all expired videos.')
+        for video in expired_videos:
+            self.stdout(f'Deleted Video:
+                            id: {video.id}, 
+                            title: {video.title}, 
+                            uplaoded: {video.uploaded}, 
+                            expires: {video.expires}, 
+                            time: {timezone.now()}\n'
+                        )
+            
+            video.delete()
+
+        self.stdout('Successfully deleted all expired videos.')
